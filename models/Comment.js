4d3e294fffe5829a2deb18b702cc2eb894cc52a0
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-09-15 15:16:36 -0500

    Add data validation for pizzas, comments, and replies

--- models/Comment.js
+++ models/Comment.js
@@ -8,10 +8,14 @@ const ReplySchema = new Schema(
       default: () => new Types.ObjectId(),
     },
     replyBody: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     writtenBy: {
       type: String,
+      required: true,
+      trim: true
     },
     createdAt: {
       type: Date,
@@ -30,9 +34,13 @@ const CommentSchema = new Schema(
   {
     writtenBy: {
       type: String,
+      required: true,
+      trim: true
     },
     commentBody: {
       type: String,
+      required: true,
+      trim: true
     },
     createdAt: {
       type: Date,
